8dc0352a80863d828ddaafcdc6c9684f0c5eb1cc
Rajual <RAJUALZAKI@GMAIL.COM>
2020-01-21 19:50:54 -0500

    initial commit

---
+++ rajual.CodeLou.ExcerciseProject/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace rajual.CodeLou.ExcerciseProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputtingStudent = true;
+            while (inputtingStudent) {
+                InputStudent();
+                Console.WriteLine("Would you like to continue inputting students? [Y/N]");
+                inputtingStudent = Console.ReadLine().ToLower() == "y";
+            }
+        }
+        static Student InputStudent()
+        {
+            Console.WriteLine("Enter Student Id");
+            var studentId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter First Name");
+            var studentFirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            var studentLastName = Console.ReadLine();
+            Console.WriteLine("Enter Class Name");
+            var className = Console.ReadLine();
+            Console.WriteLine("Enter Last Class Completed");
+            var lastClass = Console.ReadLine();
+            Console.WriteLine("Enter Last Class Completed Date in format MM/dd/YYYY");
+            var lastCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Start Date in format MM/dd/YYYY");
+            var startDate = DateTimeOffset.Parse(Console.ReadLine());
+
+            var studentRecord = new Student();
+            studentRecord.StudentId = studentId;
+            studentRecord.FirstName = studentFirstName;
+            studentRecord.LastName = studentLastName;
+            studentRecord.ClassName = className;
+            studentRecord.StartDate = startDate;
+            studentRecord.LastClassCompleted = lastClass;
+            studentRecord.LastClassCompletedOn = lastCompletedOn;
+            Console.WriteLine($"Student Id | Name |  Class "); ;
+            Console.WriteLine($"{studentRecord.StudentId} | {studentRecord.FirstName} {studentRecord.LastName} | {studentRecord.ClassName} ");
+            Console.ReadKey();    
+            return studentRecord;
+        }
+            }
+}
